b08366f7809bf644e496b870d4ebaca0edbc8b16
Jakub Nowotnik <nomeczek@gmail.com>
2023-09-10 00:14:48 +0200

    ćwiczenie 1.3

---
+++ rozdzial_1_programowanie_wspolbiezne/src/main/java/cwiczenia_1/MyThread.java
@@ -0,0 +1,17 @@
+package cwiczenia_1;
+
+/**
+ * przykładowa klasa dziedzicząca z Thread
+ */
+
+class MyThread extends Thread {
+
+    public void run() {
+        System.out.println(this.getName());
+    }
+
+    public static void main(String[] args) {
+        new MyThread().start();
+        new MyThread().start();
+    }
+}
